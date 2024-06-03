aab87445966adfaa746dc4e3ea4705f393044a94
Michal Urbanczyk <michal.urbanczyk@grapeup.com>
2024-06-03 15:33:02 +0200

    pandas added for trial

--- src/services/lambdas/get_handler.py
+++ src/services/lambdas/get_handler.py
@@ -3,6 +3,7 @@ import os
 
 import boto3
 from lambda_types import Event
+import pandas
 
 dynamodb = boto3.client("dynamodb")
 
@@ -10,7 +11,7 @@ dynamodb = boto3.client("dynamodb")
 def get_data(event: Event, lambda_context):
     table_name = os.environ["TABLE_NAME"]
     query_string_parameters = event.get("queryStringParameters")
-
+    print(pandas.__version__)
     if query_string_parameters and query_string_parameters.get("id"):
         dynamodb_item_id = query_string_parameters.get("id")
         results = dynamodb.get_item(TableName=table_name, Key={"id": {"S": dynamodb_item_id}})
