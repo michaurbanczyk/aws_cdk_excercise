3323bc499ec41e61d623b2687506e4d5042eba09
Michal Urbanczyk <michal.urbanczyk@grapeup.com>
2024-06-03 15:24:20 +0200

    poetry and pre-commit added

--- src/services/lambdas/delete_handler.py
+++ src/services/lambdas/delete_handler.py
@@ -2,25 +2,18 @@ import json
 import os
 
 import boto3
-
 from lambda_types import Event
 
-dynamodb = boto3.client('dynamodb')
+dynamodb = boto3.client("dynamodb")
 
 
 def delete_data(event: Event, lambda_context):
-    table_name = os.environ['TABLE_NAME']
+    table_name = os.environ["TABLE_NAME"]
     query_string_parameters = event.get("queryStringParameters")
 
-    if query_string_parameters and query_string_parameters.get('id'):
-        dynamodb_item_id = query_string_parameters.get('id')
-        dynamodb.delete_item(TableName=table_name, Key={'id': {"S": dynamodb_item_id}})
-        return {
-            "statusCode": 200,
-            "body": json.dumps(f"Item {dynamodb_item_id} deleted from dynamodb table!")
-        }
+    if query_string_parameters and query_string_parameters.get("id"):
+        dynamodb_item_id = query_string_parameters.get("id")
+        dynamodb.delete_item(TableName=table_name, Key={"id": {"S": dynamodb_item_id}})
+        return {"statusCode": 200, "body": json.dumps(f"Item {dynamodb_item_id} deleted from dynamodb table!")}
 
-    return {
-        "statusCode": 400,
-        "body": json.dumps("Please provide id and try again!")
-    }
+    return {"statusCode": 400, "body": json.dumps("Please provide id and try again!")}
--- src/services/lambdas/polling_handler.py
+++ src/services/lambdas/polling_handler.py
@@ -1,14 +1,10 @@
-import requests
-
 from lambda_types import Event
+import requests
 
 
 def polling_data(event: Event, lambda_context):
-    request = requests.get('https://api.github.com/')
+    request = requests.get("https://api.github.com/")
 
     print(request.text)
 
-    return {
-        'statusCode': request.status_code,
-        'body': request.text
-    }
+    return {"statusCode": request.status_code, "body": request.text}
